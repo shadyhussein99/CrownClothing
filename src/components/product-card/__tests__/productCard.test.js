6cfaf23af17cb242deb2bf7e6c5e1c4e9234daeb
shadyhussein99 <shadyhussein99@gmail.com>
2025-05-24 19:55:31 +0300

    Made a slight change to test pipeline

--- src/components/product-card/__tests__/productCard.test.js
+++ src/components/product-card/__tests__/productCard.test.js
@@ -7,31 +7,31 @@ import { renderWithProviders } from "../../../utilities/tests/test-utils";
 
 import ProductCard from "../ProductCard";
 
-
 describe("Testing Product Card component", () => {
+  test("Should add the product item when the product card button is clicked", () => {
+    // Used async because (fireEvent) method is asynchronus
+    const mockItem = {
+      id: 1,
+      imageUrl: "test",
+      name: "Item A",
+      price: 10,
+    };
 
-    test("Should add the product item when the product card button is clicked", () => { // Used async because (fireEvent) method is asynchronus
-        const mockItem = {
-            id: 1, 
-            imageUrl: "test",
-            name: "Item A",
-            price: 10
-        }
+    const { store } = renderWithProviders(<ProductCard />, {
+      // we can access the store by destructuring it from (renderWithProviders)
+      preloadedState: {
+        cart: {
+          value: [],
+        },
+        user: {
+          value: {},
+        },
+      },
+    });
 
-        const {store} = renderWithProviders(<ProductCard />, {  // we can access the store by destructuring it from (renderWithProviders)
-            preloadedState: {
-                cart: {
-                    value: []
-                },
-                user: {
-                    value: {}
-                }
-            }
-        })
+    const addToCartButton = screen.getByText("Add To Cart");
+    fireEvent.click(addToCartButton); // (fireEvent) is an object that has all (EventListeners) of javaScript
 
-        const addToCartButton = screen.getByText("Add To Cart")
-        fireEvent.click(addToCartButton)  // (fireEvent) is an object that has all (EventListeners) of javaScript
-
-        expect(store.getState().cart.value.length).toBe(1)
-    })  // the store, getState(): to access the states of the slices, slice name, required state
-})
+    expect(store.getState().cart.value.length).toBe(1);
+  }); // the store, getState(): to access the states of the slices, slice name, required state
+});
